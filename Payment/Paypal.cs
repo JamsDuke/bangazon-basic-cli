b15bca06903ead3402d485a41a8243a6e9a2c9f9
Jamie Duke <jamsduke@gmail.com>
2016-10-05 15:16:54 -0500

    initial commit

--- Payment/Paypal.cs
+++ Payment/Paypal.cs
@@ -2,18 +2,19 @@ using Bangazon.Orders;
 
 namespace Bangazon.Payments
 {
-  class Paypal: Payment {
-
-    public string email { get; set; }
-    public string password { get; set; }
-
-    public Paypal(Order order): base(order)
+    class Paypal : Payment
     {
 
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public Paypal(Order order) : base(order)
+        {
+
+        }
+        public override string process()
+        {
+            return $"You will be redirected to Paypal.com with the credentials {this.email}/{this.password}\n{base.process()}";
+        }
     }
-    public override string process() 
-    {
-      return $"You will be redirected to Paypal.com with the credentials {this.email}/{this.password}\n{base.process()}";  
-    }
-  }
 }
