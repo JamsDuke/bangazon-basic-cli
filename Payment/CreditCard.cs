b7180af7397ce5d973431a5d1c1b51df69b74058
Jamie Duke <jamsduke@gmail.com>
2016-10-06 09:12:39 -0500

    fixed merge conflicts with CreditCard.cs

--- Payment/CreditCard.cs
+++ Payment/CreditCard.cs
@@ -5,17 +5,12 @@ namespace Bangazon.Payments
   public class CreditCard: Payment 
   {
 
-        public string bankName { get; set; }
-        public string accountNumber { get; set; }
+    public string bankName { get; set; }
+    public string accountNumber { get; set; }
 
-        public CreditCard(Order order) : base(order)
-        {
+    public CreditCard(Order order): base(order)
+    {
 
-        }
-        public override string process()
-        {
-            return $"You are using a {this.bankName} card, with the account number {this.accountNumber}\n{base.process()}";
-        }
     }
     public override string process()
     {
